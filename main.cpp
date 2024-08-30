d9452914c5f85195465131e4e8f43469b7e847f3
whiterno <timofeikryuchkov@gmail.com>
2024-08-30 14:23:57 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 
+
 enum Exit{
     SUCCESS,
     FAIL
@@ -12,6 +13,16 @@ struct Matrix{
     size_t rows, cols;
 };
 
+struct Pecus{
+    int* data;
+    size_t length;
+};
+
+struct TornMatrix{
+    size_t rows;
+    Pecus* ptr_data;
+};
+
 int printMatrix(const Matrix matrix);
 int summerSchoolFootball(const int* data, const size_t teams_amount, int team1, int team2);
 void swap(int* num1, int* num2);
@@ -20,11 +31,17 @@ int rowColMult(const int* row, const int* col, const size_t first_matrix_width, 
 Matrix multiplicationMatrix(const Matrix matrix1, const Matrix matrix2);
 int printTriangleMatrix(const int* matrix, const size_t teams_amount);
 int determinant(Matrix matrix);
+int scanMatrix(Matrix* matrix);
+int scanTornMatrix(TornMatrix* tmatrix);
+void clearBuff();
+void readSpaces();
+void printTornMatrix(TornMatrix tmatrix);
 
 const size_t ROWS = 3;
 const size_t COLS = 2;
 const size_t  FOOTBALL_TEAMS_AMOUNT = 5;
 const size_t  FOOTBALL_GAMES_AMOUNT = (FOOTBALL_TEAMS_AMOUNT - 1) * FOOTBALL_TEAMS_AMOUNT / 2;
+const int MAX_INPUT = 1000000;
 
 int main(void){
     int matrix_basic1[ROWS][COLS] = {{1, 2},
@@ -55,9 +72,24 @@ int main(void){
     *(matrix_mult_result.data + 4) = 27;
     int det = determinant(matrix_mult_result);
     printf("Определитель - %d\n", det);
+    printf("\n");
+
+    // Matrix input_matrix = {};
+    // scanMatrix(&input_matrix);
+    // printMatrix(input_matrix);
+
+    TornMatrix tmatrix = {};
+    scanTornMatrix(&tmatrix);
+    printTornMatrix(tmatrix);
 
     free(matrix_sum_result.data);
+    matrix_sum_result.data = NULL;
+
     free(matrix_mult_result.data);
+    matrix_mult_result.data = NULL;
+
+    // free(input_matrix.data);
+    // input_matrix.data = NULL;
 }
 
 int printMatrix(Matrix matrix){
@@ -196,20 +228,110 @@ int determinant(Matrix matrix){
 
     for (size_t y = 0; y < matrix.rows; y++){
         Matrix minor = {.data = NULL, .rows = matrix.rows - 1, .cols = matrix.cols - 1};
-        int tmp[minor.rows][minor.cols];
+        int* tmp = (int *)calloc(minor.rows * minor.cols, sizeof(int));
         for (size_t y_minor = 0; y_minor < minor.rows; y_minor++){
             for (size_t x_minor = 0; x_minor < minor.cols; x_minor++){
                 if (y_minor < y){
-                    *((int*)tmp + y_minor * minor.cols + x_minor) = *(matrix.data + y_minor * matrix.cols + x_minor + 1);
+                    *(tmp + y_minor * minor.cols + x_minor) = *(matrix.data + y_minor * matrix.cols + x_minor + 1);
                 }
                 else{
-                    *((int*)tmp + y_minor * minor.cols + x_minor) = *(matrix.data + (y_minor + 1) * matrix.cols + x_minor + 1);
+                    *(tmp + y_minor * minor.cols + x_minor) = *(matrix.data + (y_minor + 1) * matrix.cols + x_minor + 1);
                 }
             }
         }
-        minor.data = (int*) tmp;
+        minor.data = tmp;
         result += (*(matrix.data + y * matrix.cols)) * ((y % 2 == 0) ? 1 : -1) * determinant(minor);
+        free(tmp);
+        minor.data = NULL;
     }
 
     return result;
+}
+
+int scanMatrix(Matrix* matrix){
+    assert(matrix != NULL);
+
+    char input[MAX_INPUT];
+
+    printf("Введите количество строк матрицы: \n");
+    if (scanf("%lu", &matrix->rows) != 1){
+        printf("Вы ввели неправильный тип данных\n");
+        return FAIL;
+    }
+    printf("Введите количество столбцов матрицы: \n");
+    if (scanf("%lu", &matrix->cols) != 1){
+        printf("Вы ввели неправильный тип данных\n");
+        return FAIL;
+    }
+
+    matrix->data = (int*)calloc(matrix->rows * matrix->cols, sizeof(int));
+
+    printf("Вводите значения матрицы построчно (если вы введете меньше количества столбцов,)\n");
+    printf("то на месте оставшихся элементов будет 0.\n");
+    printf("Если вы введете в строке чисел больше, чем число столбцов, то избыток не считается\n");
+    clearBuff();
+
+    for (size_t y = 0; y < matrix->rows; y++){
+        for(size_t x = 0; x < matrix->cols; x++){
+            int character;
+
+            scanf("%d", (matrix->data + y * matrix->cols + x));
+            character = getchar();
+            if (character == '\n'){
+                break;
+            }
+            ungetc(character, stdin);
+            readSpaces();
+        }
+        clearBuff();
+    }
+    return SUCCESS;
+}
+
+void clearBuff(){
+    int character;
+
+    ungetc('\n', stdin);
+    while ((character = getchar()) != '\n' && character != EOF){
+        continue;
+    }
+}
+
+void readSpaces(){
+    while (true){
+        int character = getchar();
+        if (character != ' '){
+            ungetc(character, stdin);
+            break;
+        }
+    }
+}
+
+int scanTornMatrix(TornMatrix* tmatrix){
+    printf("Введите количество строк матрицы: \n");
+    scanf("%lu", &tmatrix->rows);
+
+    tmatrix->ptr_data = (Pecus*)calloc(tmatrix->rows, sizeof(Pecus));
+    printf("Введите %lu чисел, равных длине строки: \n", tmatrix->rows);
+    for (size_t i = 0; i < tmatrix->rows; i++){
+        scanf("%lu", &(tmatrix->ptr_data + i)->length);
+    }
+
+    printf("Введите числа: \n");
+    for (size_t y = 0; y < tmatrix->rows; y++){
+        (tmatrix->ptr_data + y)->data = (int*)calloc((tmatrix->ptr_data + y)->length, sizeof(int));
+        for (size_t x = 0; x < (tmatrix->ptr_data + y)->length; x++){
+            scanf("%d", (tmatrix->ptr_data + y)->data + x);
+        }
+    }
+    return SUCCESS;
+}
+
+void printTornMatrix(TornMatrix tmatrix){
+    for (size_t y = 0; y < tmatrix.rows; y++){
+        for (size_t x = 0; x < (tmatrix.ptr_data + y)->length; x++){
+            printf("%d ", *((tmatrix.ptr_data + y)->data + x));
+        }
+        printf("\n");
+    }
 }
